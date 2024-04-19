db26ec65922e4772ecd900b1fb197c6b405bce6c
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-04-19 15:04:07 +0700

    fix: fix mobile input bug when firing gun

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,25 +14,12 @@ namespace Nightmare
         private bool isGrounded;
         private Vector3 playerVelocity;
 
-        Vector3 movement;                   // The vector to store the direction of the player's movement.
         Animator anim;                      // Reference to the animator component.
-        Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-#if !MOBILE_INPUT
-        int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-        float camRayLength = 100f;          // The length of the ray from the camera into the scene.
-#endif
 
         void Awake ()
         {
-#if !MOBILE_INPUT
-            // Create a layer mask for the floor layer.
-            floorMask = LayerMask.GetMask ("Floor");
-#endif
-
             // Set up references.
             anim = GetComponent <Animator> ();
-            playerRigidbody = GetComponent <Rigidbody> ();
-
             StartPausible();
         }
 
@@ -99,27 +86,7 @@ namespace Nightmare
 
         void Turning ()
         {
-#if !MOBILE_INPUT
 
-#else
-
-            Vector3 turnDir = new Vector3(CrossPlatformInputManager.GetAxisRaw("Mouse X") , 0f , CrossPlatformInputManager.GetAxisRaw("Mouse Y"));
-
-            if (turnDir != Vector3.zero)
-            {
-                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
-                Vector3 playerToMouse = (transform.position + turnDir) - transform.position;
-
-                // Ensure the vector is entirely along the floor plane.
-                playerToMouse.y = 0f;
-
-                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-                Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
-
-                // Set the player's rotation to this new rotation.
-                playerRigidbody.MoveRotation(newRotatation);
-            }
-#endif
         }
 
 
