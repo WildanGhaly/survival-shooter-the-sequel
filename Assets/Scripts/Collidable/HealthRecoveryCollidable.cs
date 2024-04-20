5ce3ffb8c173c61c0a824b037d8f2a6d6c2b6c0a
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-04-20 22:41:40 +0700

    feat: add health recovery orb mechanism

--- Assets/Scripts/Collidable/HealthRecoveryCollidable.cs
+++ Assets/Scripts/Collidable/HealthRecoveryCollidable.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthRecoveryCollidable : MonoBehaviour
+public class HealthRecoveryCollidable : Collidable
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void CollideEnter()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.CollideEnter();
+        HealthSystem.Instance.FullHealDamage();
     }
 }
