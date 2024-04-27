26745a300b4a286d1cc56d30f3c45b6f67b1eaf0
henry anand <13521004@std.stei.itb.ac.id>
2024-04-27 15:01:58 +0700

    feat : add quest survival logic

---
+++ Assets/Scripts/QuestSurvival/QuestSurvival.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestSurvival : MonoBehaviour
+{
+    [SerializeField] private int waktu = 120;
+    void Start()
+    {
+        StartCoroutine(OpenDoor(waktu));
+    }
+
+    void Update()
+    {
+
+    }
+
+    private IEnumerator OpenDoor(int waktu)
+    {
+        yield return new WaitForSeconds(waktu);
+        gameObject.GetComponent<Animator>().SetBool("isOpen", true);
+    }
+}
