6ba2be41b45fe6dc7599bbc1389ceb6e8df08c39
henry anand <13521004@std.stei.itb.ac.id>
2024-05-02 20:10:29 +0700

    feat : edit main scene + create interactable pet shop

---
+++ Assets/Scripts/MainScene/PetShopInteractable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class PetShopInteractable : Interactable
+{
+    [SerializeField] private GameObject UI;
+
+    protected override void Interact()
+    {
+        Debug.Log("Hello World");
+        UI.SetActive(true);
+        base.Interact();
+    }
+}
