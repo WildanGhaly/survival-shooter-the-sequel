56749fa7b8dbb330ff84cbf7eeb41d80d9c13b1c
henry anand <13521004@std.stei.itb.ac.id>
2024-05-03 23:45:12 +0700

    feat : add map ui

---
+++ Assets/Scripts/MainScene/PetItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+using static PetShopInteractable;
+
+public class PetItem : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshPro priceText;
+
+    //Setup the pet item with data
+    private void Awake()
+    {
+        nameText = new TextMeshProUGUI();
+        priceText = new TextMeshPro();
+    }
+    public void Setup(PetData data)
+    {
+        nameText.text = data.Name;
+        priceText.text = "$" + data.Price.ToString("F2");
+
+        Debug.Log("Success To Update" + nameText.text + priceText.text);
+    }
+}
--- Assets/Scripts/MainScene/PetShopInteractable.cs
+++ Assets/Scripts/MainScene/PetShopInteractable.cs
@@ -1,12 +1,58 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
 public class PetShopInteractable : Interactable
 {
     [SerializeField] private GameObject UI;
+    [SerializeField] private Transform contentPanel;
+    [SerializeField] private GameObject petItemPrefab;
+
+    private List<PetData> availablePets = new List<PetData>();
 
     protected override void Interact()
     {
-        Debug.Log("Hello World");
         UI.SetActive(true);
+        PopulatePetList();
         base.Interact();
+    }
+
+    private void PopulatePetList()
+    {
+        AddPetToShop(new PetData("Pet 1", 1231));
+        AddPetToShop(new PetData("Pet 2", 122413)); 
+        Debug.Log(availablePets.Count);
+        foreach (PetData pet in availablePets)
+        {
+            GameObject newItem = Instantiate(petItemPrefab, contentPanel);
+            TextMeshProUGUI nameText = newItem.transform.Find("NameText").GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI priceText = newItem.transform.Find("PriceText").GetComponent<TextMeshProUGUI>();
+
+            if (nameText != null && priceText != null)
+            {
+                nameText.text = pet.Name;
+                priceText.text = pet.Price.ToString();
+            }
+        }
+    }
+
+    public void AddPetToShop(PetData pet)
+    {
+        availablePets.Add(pet);
+
+    }
+
+
+    public class PetData
+    {
+        public string Name;
+        public float Price;
+
+        // ctor
+        public PetData(string _name, float _price)
+        {
+            Name = _name;
+            Price = _price;
+        }
     }
 }
