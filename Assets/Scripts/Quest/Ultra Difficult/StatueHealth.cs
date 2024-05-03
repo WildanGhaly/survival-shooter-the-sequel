faee5237fad413655b58369f7f5f56e24de37e67
Mahen_Matt <13521007@std.stei.itb.ac.id>
2024-05-03 22:02:40 +0700

    feat: Cutscene of Scene

--- Assets/Scripts/Quest/Ultra Difficult/StatueHealth.cs
+++ Assets/Scripts/Quest/Ultra Difficult/StatueHealth.cs
@@ -4,13 +4,22 @@ using Nightmare;
 using UnityEngine.SceneManagement;
 public class StatueHealth : EnemyHealth
 {
-    [SerializeField] private CanvasGroup canvasGroupBlack;
-    [SerializeField] private CanvasGroup canvasGroupText;
-    [SerializeField] private GameObject player;
+    [SerializeField] CanvasGroup canvasGroupText;
+    [SerializeField] GameObject player;
+    [SerializeField] RectTransform healthBar;
+    private float healthBarWidth;
+    private float healthMultiplierToUI;
 
     public GameObject newCam;
     private float fadeDuration = 0.5f;
-    
+
+    protected override void Awake()
+    {
+        base.Awake();
+        healthBarWidth = healthBar.sizeDelta.x;
+        healthMultiplierToUI = healthBarWidth / 10000;
+    }
+
     protected override void Death()
     {
         Debug.Log("DESTROYED STATUE");
@@ -35,6 +44,7 @@ public class StatueHealth : EnemyHealth
         {
             base.enemyAudio.Play();
             currentHealth -= amount;
+            healthBar.sizeDelta = new Vector2(healthBar.sizeDelta.x - amount * healthMultiplierToUI, healthBar.sizeDelta.y);
 
             if (IsDead())
             {
@@ -45,12 +55,14 @@ public class StatueHealth : EnemyHealth
 
     private IEnumerator fadeToBlack()
     {
-        float fadeSpeed = (float) Mathf.Abs(canvasGroupBlack.alpha-1)/fadeDuration;
-        while (canvasGroupBlack.alpha != 1)
-        {
-            canvasGroupBlack.alpha = Mathf.MoveTowards(canvasGroupBlack.alpha, 1, fadeSpeed * Time.deltaTime);
-            yield return null;
-        }
+        float fadeSpeed = (float) 1/fadeDuration;
+        // while (canvasGroupBlack.alpha != 1)
+        // {
+        //     canvasGroupBlack.alpha = Mathf.MoveTowards(canvasGroupBlack.alpha, 1, fadeSpeed * Time.deltaTime);
+        //     yield return null;
+        // }
+
+        SwitchCamera.Instance.SimpleFade(1, 0.5f);
 
         while (canvasGroupText.alpha != 1)
         {
@@ -58,13 +70,15 @@ public class StatueHealth : EnemyHealth
             yield return null;
         }
 
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(5);
 
         while (canvasGroupText.alpha != 0)
         {
             canvasGroupText.alpha = Mathf.MoveTowards(canvasGroupText.alpha, 0, fadeSpeed * Time.deltaTime);
             yield return null;
         }
+
+        yield return new WaitForSeconds(5);
 
         SceneManager.LoadScene(0); // TODO: Go to main scene
     }
