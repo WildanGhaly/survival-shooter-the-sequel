21f4680c94a380e3c21343325b5f190cc860724c
Mahen_Matt <13521007@std.stei.itb.ac.id>
2024-05-06 12:36:14 +0700

    chore: Refactor Statistic Display

---
+++ Assets/Scripts/Managers/PlayerStatisticGUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerStatisticGUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public TextMeshProUGUI playerNameValue;
+    [SerializeField] public TextMeshProUGUI killCountValue;
+    [SerializeField] public TextMeshProUGUI distanceValue;
+    [SerializeField] public TextMeshProUGUI hitRatioValue;
+    [SerializeField] public TextMeshProUGUI timePlayedValue;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerNameValue.SetText(PlayerStatistic.INSTANCE.getPlayerName().ToString() + "'s statistics");
+        killCountValue.SetText(PlayerStatistic.INSTANCE.getKillCount().ToString());
+        distanceValue.SetText(PlayerStatistic.INSTANCE.getDistance().ToString());
+        hitRatioValue.SetText(PlayerStatistic.INSTANCE.getHitRatio().ToString());
+        timePlayedValue.SetText(((int) PlayerStatistic.INSTANCE.getTimePlayed()/60).ToString()+ ":"+((int)PlayerStatistic.INSTANCE.getTimePlayed()%60).ToString());
+    }
+}
