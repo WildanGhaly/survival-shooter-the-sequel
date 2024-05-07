97f3d21d2d3cb7344947b9b69d89576ba7d1ce0e
haikalardzi <haikalardzi@gmail.com>
2024-05-07 20:30:56 +0700

    feat: 45% scene 5

---
+++ Assets/Scripts/Quest/Rescue/EnemyManagerScene5.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace Nightmare
+{
+    public class SpawnOnce : PausibleObject
+    {
+        public GameObject enemy;
+        public float spawnTime = 3f;
+        public Transform[] spawnPoints;
+
+        private float timer;
+        private bool isSpawned;
+
+        void Start ()
+        {
+            timer = spawnTime;
+            isSpawned = false;
+        }
+
+        void OnEnable()
+        {
+            if(HealthSystem.Instance.hitPoint <= 0f)
+            {
+                StartPausible();
+            }
+        }
+
+        void OnDestroy()
+        {
+            StopPausible();
+        }
+
+        void Update()
+        {
+            if (isPaused)
+                return;
+
+            timer -= Time.deltaTime;
+            if (timer <= 0f & !isSpawned)
+            {
+                Spawn();
+                isSpawned = true;
+            }
+        }
+
+        void Spawn ()
+        {
+            // If the player has no health left...
+            if(HealthSystem.Instance.hitPoint <= 0f)
+            {
+                // ... exit the function.
+                return;
+            }
+
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            
+            Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        }
+    }
+}
