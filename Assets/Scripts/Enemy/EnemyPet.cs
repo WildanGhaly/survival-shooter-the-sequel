f583b52b3325fe3592288f965850a3c52d1d488d
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-05-09 07:59:43 +0700

    feat: add slime pet with movement logic for increase enemy damage

---
+++ Assets/Scripts/Enemy/EnemyPet.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPet : MonoBehaviour
+{
+
+    [SerializeField] private float damageIncreasePercent = 20f;
+    [SerializeField] private float radiusFromEnemy = 10f;
+    [SerializeField] private float petSpeed = 5f;
+
+    private GameObject player;
+    private Transform pointA; // player
+    private Transform pointB; // enemy
+    private Vector3 pointC; // target
+    private Transform pointD; // slime
+    private Animator animate;
+
+    public GameObject enemy;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pointA = player.transform;
+        pointB = enemy.transform;
+        pointC = Vector3.zero;
+        pointD = transform;
+        animate = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        GetPositionC(pointA, pointB);
+        LookToC(pointC, pointD);
+        float distanceToPlayer = Vector3.Distance(pointA.position, pointD.position);
+        float distanceToEnemy = Vector3.Distance(pointB.position, pointD.position);
+        
+        if (distanceToEnemy < radiusFromEnemy && distanceToPlayer > radiusFromEnemy)
+        {
+            Stay();
+        }
+        else
+        {
+            Run();
+        }
+    }
+
+    void GetPositionC(Transform pointA, Transform pointB)
+    {
+        float distanceToPlayer = Vector3.Distance(pointA.position, pointD.position);
+        float distanceToEnemy = Vector3.Distance(pointB.position, pointD.position);
+
+        if (distanceToEnemy < distanceToPlayer)
+        {
+            Vector3 direction = pointB.position - pointA.position;
+            Vector3 normalizedDirection = direction.normalized;
+            pointC = pointB.position + normalizedDirection * radiusFromEnemy;
+        }
+        else if (distanceToEnemy > radiusFromEnemy)
+        {
+            Vector3 direction = pointD.position - pointA.position;
+            Vector3 normalizedDirection = direction.normalized;
+            pointC = pointD.position + normalizedDirection * radiusFromEnemy;
+        }
+        else
+        {
+            Vector3 direction = pointA.position - pointD.position;
+            Vector3 normalizedDirection = direction.normalized;
+            pointC = pointA.position + normalizedDirection * radiusFromEnemy;
+        }
+    }
+
+    void LookToC(Vector3 pointC, Transform pointD)
+    {
+        Vector3 direction = pointC - pointD.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        Quaternion yRotation = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
+        pointD.rotation = yRotation;
+    }
+
+    void Run()
+    {
+        animate.SetFloat("Speed", petSpeed);
+    }
+    
+    void Stay()
+    {
+        animate.SetFloat("Speed", 0);
+    }
+}
---
+++ Assets/Scripts/Enemy/InstantiatePet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatePet : MonoBehaviour
+{
+    [SerializeField] private GameObject pet;
+    [SerializeField] private int numberOfPet = 1;
+    [SerializeField] private float spawnRadius = 5.0f;
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfPet; i++)
+        {
+            Vector3 randomPosition = Random.insideUnitSphere * spawnRadius;
+            randomPosition.y = 0;
+            Vector3 spawnPosition = transform.position + randomPosition;
+            
+            GameObject enemyPet = Instantiate(pet, spawnPosition, Quaternion.identity);
+            enemyPet.GetComponent<EnemyPet>().enemy = gameObject;
+        }
+    }
+}
