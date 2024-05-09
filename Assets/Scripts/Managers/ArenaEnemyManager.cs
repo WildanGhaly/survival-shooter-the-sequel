eb3f9900ca893f0263ed529eccfde6c4cfed98a1
Christophorus Dharma Winata <13521009@std.stei.itb.ac.id>
2024-05-09 14:14:27 +0700

    feat!!: Parkour scene

--- Assets/Scripts/Managers/ArenaEnemyManager.cs
+++ Assets/Scripts/Managers/ArenaEnemyManager.cs
@@ -7,14 +7,19 @@ namespace Nightmare
     public class ArenaEnemyManager : VariableEnemyManager
     {
         public int targetSpawned = 50;
+        public bool isDoneSpawning = false;
         protected override void Spawn()
         {
             if (spawned < targetSpawned)
             {
                 base.Spawn();
                 spawned++;
+            } else
+            {
+                isDoneSpawning = true;
+                Debug.Log("Enemy is all eliminated!");
+                enabled = false;
             }
-            Debug.Log("Enemy is all eliminated!");
         }
     }
 }
